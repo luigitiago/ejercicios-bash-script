3b23b5cae4cbcf1bd72bb26e985d5367a219f80b
Lexus <luigisantiago1994@gmail.com>
2026-09-24 00:22:00 +0200

    Add fecha-directorio.sh

---
+++ fecha-directorio.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Nombre: fecha-directorio.sh
+# Descripcion: Muestra la fecha, el calendario  actual y el directorio
+
+echo "=== Informacion del Sistema ==="
+echo "Calendario:"
+cal
+
+echo "Fecha y hora acual:"
+date
+
+echo ""
+
+echo "Directorio actual:"
+pwd
