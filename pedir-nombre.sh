d77a87c8134cc9298dba5898e596ab10c34797c5
Lexus <luigisantiago1994@gmail.com>
2026-09-25 14:31:42 +0200

    Add pedir-nombre.sh script

---
+++ pedir-nombre.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Nombre: pedir-nombre.sh
+#Descripcion: Pide el nombre al usuario
+read -p "Por favor introduce tu nombre:" nombre
+echo "Hola, $nombre!"
+echo ""
+echo "Encantado de conocerte, ${nombre}!"
